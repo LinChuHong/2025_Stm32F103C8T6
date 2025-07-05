e3fa3fd2b52f3c6610661c8345e2e2ce3296af49
6LiuLin6 <964449120liu@gmail.com>
2025-07-05 11:34:48 +0800

    控制电动24V推杆去测试1出110UDP卡

--- src/main.cpp
+++ src/main.cpp
@@ -4,16 +4,21 @@
 #define RELAY2 PA1
 #define SWITCH1 PB12
 #define SWITCH1GROUND PB13
+#define TOSTARTSWITCH PB14
+
+
+
 OneButton button(SWITCH1, true);
 
 uint8_t SWITCH1State = 0;
-
+int buttonState;
 
 
 
 void buttonInit()
 {
-    
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(TOSTARTSWITCH, INPUT_PULLUP);
     pinMode(RELAY1, OUTPUT);
     pinMode(RELAY2, OUTPUT);
     pinMode(SWITCH1, INPUT_PULLUP);
@@ -21,34 +26,61 @@ void buttonInit()
     digitalWrite(SWITCH1GROUND, LOW);
     button.attachPress([]() 
     {
-        if (SWITCH1State == 0)
+        if (buttonState == LOW)
         {
-            digitalWrite(RELAY1, HIGH);
-            delay(30);
-            digitalWrite(RELAY1, LOW);
-            delay(30);
-            SWITCH1State = 1;
-        }
-        else if (SWITCH1State == 1)
-        {
-            digitalWrite(RELAY2, HIGH);
-            delay(30);
-            digitalWrite(RELAY2, LOW);
-            delay(30);
-            SWITCH1State = 2;
-        }
-        else if (SWITCH1State == 2) 
-        {
-            digitalWrite(RELAY1, HIGH);
-            digitalWrite(RELAY2, HIGH);
-            delay(30);
-            digitalWrite(RELAY1, LOW);
-            digitalWrite(RELAY2, LOW);
-            delay(30);
-            SWITCH1State = 0;
+            if (SWITCH1State == 0)
+            {
+                digitalToggle(LED_BUILTIN);
+                digitalWrite(RELAY1,HIGH);
+                digitalWrite(RELAY2,HIGH);
+                delay(30);
+                digitalWrite(RELAY1,LOW);
+                digitalWrite(RELAY2,LOW);
+                delay(30);
 
 
+                SWITCH1State = 1;
+            }
+            else if (SWITCH1State == 1)
+            {
+
+                digitalToggle(LED_BUILTIN);
+                digitalWrite(RELAY1,HIGH);
+                digitalWrite(RELAY2,HIGH);
+                delay(30);
+                digitalWrite(RELAY1,LOW);
+                digitalWrite(RELAY2,LOW);
+                delay(30);
+                SWITCH1State = 0;
+            }
         }
+        // if (SWITCH1State == 0)
+        // {
+        //     digitalToggle(LED_BUILTIN);
+        //     digitalWrite(RELAY1,HIGH);
+        //     digitalWrite(RELAY2,HIGH);
+        //     delay(30);
+        //     digitalWrite(RELAY1,LOW);
+        //     digitalWrite(RELAY2,LOW);
+        //     delay(30);
+
+
+        //     SWITCH1State = 1;
+        // }
+        // else if (SWITCH1State == 1)
+        // {
+
+        //     digitalToggle(LED_BUILTIN);
+        //     digitalWrite(RELAY1,HIGH);
+        //     digitalWrite(RELAY2,HIGH);
+        //     delay(30);
+        //     digitalWrite(RELAY1,LOW);
+        //     digitalWrite(RELAY2,LOW);
+        //     delay(30);
+        //     SWITCH1State = 0;
+        // }
+
+        
     }
     );
 }
@@ -63,5 +95,7 @@ void setup()
 void loop() 
 {
     button.tick();
+    buttonState = digitalRead(TOSTARTSWITCH);
+    Serial.println(buttonState);
 }
 
